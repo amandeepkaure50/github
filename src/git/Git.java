11c3c95b8d1a9378cf69ff5e487f7c3e35351216
amandeepkaure50 <200332905@student.georgianc.on.ca>
2017-02-10 11:55:11 -0500

    intilizing the git

---
+++ src/git/Git.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author Amandeep
+ */
+public class Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Wahoo - I have created a repository in GitHub");
+        
+        System.out.println("Here is a change to the repository");
+    }
+    
+}
